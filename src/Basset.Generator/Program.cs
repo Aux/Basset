82279893b49582aeedf75f478897a5ccbc8de9c0
Aux <justin@akitaux.com>
2020-05-24 17:15:58 -0500

    Create generator project

---
+++ src/Basset.Generator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Basset.Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
