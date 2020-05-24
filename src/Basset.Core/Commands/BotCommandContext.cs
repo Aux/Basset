ebefe9ab61a1c436200a28f9888642b6e48dfa52
Aux <justin@akitaux.com>
2020-05-24 17:16:15 -0500

    Add command items

---
+++ src/Basset.Core/Commands/BotCommandContext.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Basset
+{
+    public class BotCommandContext : ICommandContext
+    {
+        public DiscordShardedClient Client { get; }
+        public SocketGuild Guild { get; }
+        public ISocketMessageChannel Channel { get; }
+        public SocketUser User { get; }
+        public SocketUserMessage Message { get; }
+
+        public bool IsPrivate => Channel is IPrivateChannel;
+
+        public BotCommandContext(DiscordShardedClient client, SocketUserMessage msg, SocketUser user = null)
+        {
+            Client = client;
+            Guild = (msg.Channel as SocketGuildChannel)?.Guild;
+            Channel = msg.Channel;
+            User = user ?? msg.Author;
+            Message = msg;
+        }
+
+        //ICommandContext
+        IDiscordClient ICommandContext.Client => Client;
+        IGuild ICommandContext.Guild => Guild;
+        IMessageChannel ICommandContext.Channel => Channel;
+        IUser ICommandContext.User => User;
+        IUserMessage ICommandContext.Message => Message;
+    }
+}
---
+++ src/Basset.Core/Commands/BotModuleBase.cs
@@ -0,0 +1,12 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Basset
+{
+    public abstract class BotModuleBase : ModuleBase<BotCommandContext>
+    {
+        public Task ReplyAsync(Embed embed, RequestOptions options = null)
+            => ReplyAsync("", false, embed, options);
+    }
+}
