6c562631a179d1fb8ab91546c6d21638f2c482b2
Aux <justin@akitaux.com>
2020-05-24 17:15:43 -0500

    Create collector skaffolding

---
+++ src/Basset.Collector/CollectingService.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Basset.Collector
+{
+    public class CollectingService
+    {
+        private readonly ILogger<CollectingService> _logger;
+        private readonly IConfiguration _config;
+        private readonly DiscordShardedClient _discord;
+
+        public CollectingService(ILogger<CollectingService> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+            _discord = new DiscordShardedClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+        }
+
+        public async Task RunAsync()
+        {
+            _logger.LogInformation("Starting");
+            _discord.GuildMemberUpdated += OnGuildMemberUpdatedAsync;
+
+            await _discord.LoginAsync(TokenType.Bot, _config["discord:token"]);
+            await _discord.StartAsync();
+            _logger.LogInformation("Running");
+        }
+
+        private Task OnGuildMemberUpdatedAsync(SocketGuildUser before, SocketGuildUser after)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Basset.Collector/Program.cs
@@ -0,0 +1,30 @@
+﻿using Basset.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Basset.Collector
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory, "common"))
+                .AddYamlFile("config.yml")
+                .AddCommandLine(args)
+                .Build();
+
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddProvider(new BotLoggerProvider(config));
+            });
+            var logger = loggerFactory.CreateLogger<CollectingService>();
+
+            var collector = new CollectingService(logger, config);
+            await collector.RunAsync();
+        }
+    }
+}
